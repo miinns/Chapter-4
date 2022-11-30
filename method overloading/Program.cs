d43e5b67e8e31afd3890d06de31af1f87e6ffced
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 01:00:53 +0900

    Update Program.cs

--- method overloading/Program.cs
+++ method overloading/Program.cs
@@ -19,7 +19,7 @@ class MethodOverloadingApp
     {
         Console.WriteLine("SomeThing(double) is called.");
     }
-    // 객체 생성
+    // 객체 생
     public static void Main()
     {
         MethodOverloadingApp obj = new MethodOverloadingApp();
